ed6c120d741491bc277abb33be640ac74b4e803f
ArmanDipu <armanhossain5@iut-dhaka.edu>
2022-01-14 14:15:59 +0600

    Create SignInPage.vue

---
+++ 1-Project/frontend/src/modules/auth/components/SignInPage.vue
@@ -0,0 +1,169 @@
+<template>
+  <v-app id="inspire" >
+    <v-content class ="hero">
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-3"
+                        >Virtual Library Assistant</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4" >Sign in as :
+                          <button class="link" >Librarian</button>
+                          <button class="link" >Student</button>
+                          </h4>
+                        <v-form>
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                          <h4 class="text-center mt-4" > Remember Me</h4>
+                        <div class="link">
+                        <h4 class="text-center mt-4">Forgot password ?</h4>
+                        </div>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                      </div>
+                      <br>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hello!</h1>
+                        <br>
+                        <h4
+                          class="text-center"
+                        >Register for a new Account</h4>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back!</h1>
+                        <h5
+                          class="text-center"
+                        >To use Virtual Library Assistant please login with your personnel info</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Provide all necessary info for registration</h4>
+                        <br>
+                        <v-form>
+                          <v-text-field
+                            label="Name"
+                            name="Name"
+                            prepend-icon="person"
+                            type="text"
+                            color="teal accent-3"
+                          />
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <br>
+                      <div class="text-center mt-n5">
+                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
+                      </div>
+                      <br>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
+
+<style scoped>
+  .link{
+    padding-left: 5px;
+    padding-right: 5px;
+    color: orangered;
+  }
+  .hero {
+  background: url('https://images.unsplash.com/photo-1593071045320-171ec7573b11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1162&q=80');
+  background-size: cover;
+  }
+</style>
