bc46a1a9487e2bddbae86342d2af2c45ab277617
ArmanDipu <armanhossain5@iut-dhaka.edu>
2022-01-14 13:38:50 +0600

    created fronted project with Vue framework

---
+++ 1-Project/frontend/src/app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" v-if="!loadingInit">
+    <transition mode="out-in" name="fade">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'app',
+
+  methods: {
+    ...mapActions({
+      doInit: 'auth/doInit',
+      resize: 'layout/resize',
+    }),
+
+    handleResize() {
+      this.resize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    },
+  },
+
+  computed: {
+    ...mapGetters({
+      loadingInit: 'auth/loadingInit',
+    }),
+  },
+};
+</script>
+
+
+<style>
+
+</style>
